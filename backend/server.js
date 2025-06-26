f91c704c5b2312d129f09feb35ad83ddea6162d9
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-26 11:20:08 -0700

    Configured the session to allow usage

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+require('dotenv').config();
+
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+let sessionConfig = {
+    name: 'sessionId',
+    secret: process.env.SESSION_SECRET,
+    cookie: { secure: false },
+    resave: false,
+    saveUninitialized: false
+}
+
+app.use(session(sessionConfig))
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 8000;
