1f1975da2bd3d814d9049751a2c2f161f643d502
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-27 13:05:53 -0700

    Implemented an authentication HOC to make page access available for only logged in users

---
+++ frontend/src/components/auth/WithAuth.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useUser } from "../../contexts/UserContext";
+
+export default function WithAuth(WrappedComponent, authRoles) {
+
+    return function ProtectedComponent(props) {
+        const { user, loading } = useUser();
+        const navigate = useNavigate();
+
+        useEffect(() => {
+            if (!loading && (!user && !authRoles.includes(user.role))) 
+                navigate('/auth/login');
+        }, [user, loading, navigate]);
+
+        if(loading) return <p>Loading...</p>
+        if (!user && !authRoles.includes(user.role)) return null;
+
+        return <WrappedComponent {...props} />;
+    };
+};
