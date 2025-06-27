ff18b25460161003dafa7e8efd69be60974d5b61
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-27 16:38:28 -0700

    Added the Header component into pages

--- frontend/src/pages/Welcome/WelcomePage.jsx
+++ frontend/src/pages/Welcome/WelcomePage.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom"
+import Header from "../../components/Header/Header";
 
 export default function WelcomePage() {
     const navigate = useNavigate();
     const go = role => navigate('/auth/signup', {state: {role} });
     return (
         <div>
+            <Header />
             <h1>Welcome to MentorMe</h1>
             <h2>Get Started!</h2>
             <button onClick={() => go('mentor')}>Mentor</button>
