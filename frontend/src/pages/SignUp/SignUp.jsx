1d960937b24cbe6f09244fb7415aaf2560e35436
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-30 13:09:54 -0700

    Created an array of predefined interests to suit design

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import { useNavigate, useLocation, Link } from "react-router-dom"
 import { registerUser } from "../../services/dataService";
 import { useUser } from "../../contexts/UserContext";
+import "./SignUp.css"
+import logo from "../../assets/logo.png"
 
 export default function SignUp() {
     const [name, setName] = useState("");
@@ -11,16 +13,23 @@ export default function SignUp() {
     const [email, setEmail] = useState("");
     const [school, setSchool] = useState("");
     const [password, setPassword] = useState("");
-    const [description, setDescription] = useState("");
     const [availability, setAvailability] = useState("");
     const [bio, setBio] = useState("");
+    const [interests, setInterests] = useState([]);
+    const [description, setDescription] = useState("");
     const [error, setError] = useState("");
 
     const { setUser } = useUser();
-
     const navigate = useNavigate();
     const { state } = useLocation();
     const role = state?.role;
+
+    const popularSkills = [
+        'Data Science',
+        'Public Relations',
+        'UI/UX Design',
+        'Technical Interview'
+    ]
 
     const handleSignUp = async (e) => {
         e.preventDefault();
@@ -33,88 +42,58 @@ export default function SignUp() {
                 school,
                 major,
                 classification,
-                description,
+                interests,
                 bio,
                 availability
             });
-
             setUser(data);
             navigate('/auth/login');
         } catch (err) {
-            setError(err.message || "Error occured signing you in. Try again!");
+            setError("Error occured creating an account. Try again!");
         }
     };
 
     return (
-        <div className="min-h-screen flex bg-gradient-to-l from-[#EDE8F5] via-[#EDE8F5] to-[#ADBBDA]">
+        <div className="signup">
             {error && <div className="error">{error}</div>}
-            <div className="w-1/3 bg-gray-100 flex flex-col justify-center p-6">
-                <h1 className="text-2xl font-bold mb-1">MentorMe</h1>
+
+            <div className="left-side">
+                <img src={logo} />
                 <h2 className="text-3xl font-bold mb-4">Create An Account</h2>
-                <p className="text-gray-600">Join our community and start your jouney towards growth and success</p>
+                <p className="text-gray-600">
+                    Join our community and start your jouney towards growth and success
+                </p>
             </div>
 
-            <form className="w-2/3 flex flex-col justify-center px-16 py-5" onSubmit={handleSignUp}>
+            <form onSubmit={handleSignUp}>
 
                 <h3 className="text-xl font-bold mb-3">Personal Information</h3>
-                <div className="grid grid-cols-2 gap-4">
-                    <label className="block mb-1 font-medium">Full Name
-                        <input
-                            type="text"
-                            className="w-full rounded px-2 py-1 text-sm shadow-lg"
-                            placeholder="John Doe"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required />
+                <div className="content">
+                    <label>Full Name
+                        <input type="text" placeholder="John Doe" value={name} onChange={(e) => setName(e.target.value)} required />
                     </label>
-                    <label className="block mb-1 font-medium">School Email
-                        <input
-                            type="text"
-                            className="w-full rounded px-2 py-1 text-sm shadow-xl"
-                            placeholder="john.doe@example.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required />
+                    <label>School Email
+                        <input type="email" placeholder="john.doe@example.com" value={email} onChange={(e) => setEmail(e.target.value)} required />
                     </label>
                 </div>
                 <div className="mt-4">
-                    <label className="block mb-1 font-medium">Password
-                        <input
-                            type="password"
-                            className="w-full rounded px-2 py-1 text-sm shadow-xl"
-                            placeholder="Enter your password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required />
+                    <label>Password
+                        <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                     </label>
                 </div>
 
-                <h3 className="text-xl font-bold mt-8 mb-3">Academic Details</h3>
+                <h3 className="section-title">Academic Details</h3>
                 <div>
-                    <label className="block mb-1 font-medium">University/College
-                        <input
-                            type="text"
-                            className="w-full rounded px-2 py-1 text-sm shadow-xl"
-                            placeholder="Search your school..."
-                            value={school}
-                            onChange={(e) => setSchool(e.target.value)}
-                            required />
+                    <label>University/College
+                        <input type="text" placeholder="Search your school..." value={school} onChange={(e) => setSchool(e.target.value)} required />
                     </label>
-                    <div className=" mt-4 grid grid-cols-2 gap-4">
-                        <label className="block mb-1 font-medium">Major
-                            <input
-                                type="text"
-                                className="w-full rounded px-2 py-1 text-sm shadow-xl"
-                                placeholder="Enter your major"
-                                value={major}
-                                onChange={(e) => setMajor(e.target.value)}
-                                required />
+                    <div className="mt-4 content">
+                        <label>Major
+                            <input type="text" placeholder="Enter your major" value={major} onChange={(e) => setMajor(e.target.value)} required />
                         </label>
 
-                        <label className="block mb-1 font-medium">Classification
-                            <select className="w-full rounded px-2 py-1 text-sm shadow-xl"
-                                value={classification} onChange={(e) => setClassification(e.target.value)}
-                                required>
+                        <label>Classification
+                            <select value={classification} onChange={(e) => setClassification(e.target.value)} required>
                                 <option value="">Select your classification</option>
                                 <option value="Freshman">Freshman</option>
                                 <option value="Sophomore">Sophomore</option>
@@ -125,58 +104,87 @@ export default function SignUp() {
                     </div>
                 </div>
 
-                <h3 className="text-xl font-bold mt-8 mb-3">Tell Us About Yourself</h3>
+                <h3 className="section-title">Tell Us About Yourself</h3>
                 <div>
-                    <label className="block mb-1 font-medium">Short Bio
-                        <textarea
-                            className="w-full rounded px-2 py-1 text-sm shadow-xl"
-                            placeholder="Share a brief overview of your background, aspirations, and what you hope to gain from mentorship"
-                            maxLength="255" rows="3"
-                            value={bio}
-                            onChange={(e) => setBio(e.target.value)}
-                            required></textarea>
+                    <label>Short Bio
+                        <textarea placeholder="Share a brief overview of your background, aspirations, and what you hope to gain from mentorship"
+                            maxLength="255" rows="3" value={bio} onChange={(e) => setBio(e.target.value)} required>
+                        </textarea>
                     </label>
+
                     <div className="mt-4">
-                        <label className="block mb-1 font-medium">Interest & Experience
-                            <div className="flex gap-2 mb-2">
-                                <input
-                                    type="text"
-                                    className="w-full rounded px-2 py-1 text-sm shadow-xl"
-                                    placeholder="Add custom interest or skill (e.g. Python, Leadership)"
-                                    value={description}
-                                    onChange={(e) => setDescription(e.target.value)}
-                                    required />
-                                <button className="bg-gray-300 px-3 py-2 rounded"><BiPlus /></button>
-                            </div>
-                        </label>
+                        <label>Interest & Experience</label>
+                        <div className="flex gap-2 mb-2">
+                            <input type="text" placeholder="Add custom interest or skill (e.g. Python, Leadership)"
+                                value={description} onChange={(e) => setDescription(e.target.value)}
+                            />
+                            <button type="button"
+                                className="border rounded px-2 py-1 text-sm shadow-xl"
+                                onClick={() => {
+                                    if (description && !interests.includes(description)) {
+                                        setInterests([...interests, description]);
+                                        setDescription('');
+                                    }
+                                }}>
+                                <BiPlus />
+                            </button>
+                        </div>
+
                         <p className="text-gray-500 font-medium">Choose from popular skills</p>
-                        <div className="flex flex-wrap gap-2">
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Data Science <span className="text-sm">&times;</span></span>
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">UI/UX Design <span className="text-sm">&times;</span></span>
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Public Relations <span className="text-sm">&times;</span></span>
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Software Development <span className="text-sm">&times;</span></span>
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Technical Interview <span className="text-sm">&times;</span></span>
+                        <div className="flex flex-wrap gap-2 mb-2">
+                            {popularSkills.map((tag) => (
+                                <span
+                                    key={tag}
+                                    onClick={() => {
+                                        if (!interests.includes(tag)) {
+                                            setInterests([...interests, tag]);
+                                        }
+                                    }}
+                                    className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer hover:bg-[#ADBBDA]">
+                                    {tag}
+                                </span>
+                            ))}
                         </div>
+                        
+                        {interests.length > 0 && (
+                            <div className="mt-2 flex flex-wrap gap-2">
+                                {interests.map((interest) => (
+                                    <div key={interest}
+                                        className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">
+                                        <span className="mr-2">{interest}</span>
+                                        <button type="button"
+                                            onClick={() => {
+                                                setInterests(interests.filter((item) => item !== interest))
+                                            }}
+                                            className="text-white-500 font-bold cursor-pointer hover:text-gray-700"
+                                        >&times;
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+
                     </div>
-                    <div className="mt-8">
-                        <label className="block mb-1 font-medium">Availability(Days)
-                            <input
-                                type="text"
-                                className="w-full rounded px-2 py-1 text-sm shadow-xl"
-                                value={availability}
-                                onChange={(e) => setAvailability(e.target.value)}
-                                required />
-                        </label>
-                        <div className="flex flex-wrap gap-2">
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Monday </span>
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Tuesday</span>
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Wednesday</span>
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Thursday</span>
-                            <span className="bg-[#8697C4] text-white px-3 py-1 rounded-full cursor-pointer">Friday</span>
-                        </div>
+
+                    <label>Availability(Days) </label>
+                    <div className=" flex gap-2 flex-wrap items-center">
+                        {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'].map((day) => (
+                            <button
+                                type="button"
+                                key={day}
+                                onClick={() => setAvailability((prev) =>
+                                    prev.includes(day) ? prev.filter((d) => d !== day) : [...prev, day]
+                                )
+                                }
+                                className={`avail ${availability.includes(day) ?
+                                    'bg-[#8697C4] text-white px-3 py-1 rounded-full' : 'bg-white border-gray-300'}`}
+                            >
+                                {day}
+                            </button>
+                        ))}
                     </div>
                 </div>
-                <button className="font-medium bg-[#7091E6] px-6 py-3 rounded mt-8 hover:bg-[#3D52A0] hover:text-white hover:cursor-pointer" type="submit">Create an Account</button>
+                <button className="btn-submit" type="submit">Create an Account</button>
                 <p className="mt-4 text-gray-600">Already have an account? <Link className="font-medium text-[#3D52A0]" to="/auth/login">Sign In</Link></p>
             </form>
         </div>
