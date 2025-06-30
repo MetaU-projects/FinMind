6d047f245bb7b754cb5c7acd803c35735bb3e95f
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-06-29 17:03:04 -0700

    Created connections page

---
+++ frontend/src/pages/Connections/Connections.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header/Header";
+
+export default function Connections() {
+    return (
+        <div>
+            <Header />
+            <h1>Shows connections for either mentors or mentees</h1>
+        </div>
+    )
+}
