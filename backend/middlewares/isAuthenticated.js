b71878c8fd969ce485eb1aea5f022d8595e36a59
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-01 14:27:51 -0700

    Created an authentication middleware for the backend

---
+++ backend/middlewares/isAuthenticated.js
@@ -0,0 +1,8 @@
+const isAuthenticated = (req, res, next) => {
+    if(!req.session.userId) {
+        return res.status(401).json({error: "You must be logged in to perform this action."});
+    }
+    next();
+}
+
+module.exports = isAuthenticated;
