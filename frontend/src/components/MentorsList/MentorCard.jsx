354fe78c5cff353e0dbca029ec905be1923d5d01
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-02 09:55:30 -0700

    Created a component that displays the content of a mentor and styled it to follow the mockup

---
+++ frontend/src/components/MentorsList/MentorCard.jsx
@@ -0,0 +1,31 @@
+import { RxAvatar } from "react-icons/rx"; 
+import './MentorCard.css';
+
+export default function MentorCard({ mentor }) {
+    return (
+        <div className='card'>
+            <div className='card-left'>
+                <div className="card-icon">
+                    <RxAvatar className="avatar" />
+                    <h4>{mentor.classification}</h4>
+                </div>
+                <div className="mentor-detail">
+                    <div className="top">
+                        <h3>{mentor.name}</h3>
+                        <div className="skill-tags">
+                            {mentor.description.split(', ').map(skill => (
+                                <span key={skill} className="skill-tag">{skill}</span>
+                            ))}
+                        </div>
+                    </div>
+                    <p>{mentor.bio}</p>
+                </div>
+            </div>
+            
+            <div className="card-right">
+                <p>{mentor.major}</p>
+                <button className="connect-btn">Connect</button>
+            </div>
+        </div>
+    )
+}
