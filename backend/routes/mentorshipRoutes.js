a47e75828a47066ce8d3f53a8ba82b0dd313236b
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-02 14:48:54 -0700

    Added routes to the controller functions in order interact with the server

--- backend/routes/mentorshipRoutes.js
+++ backend/routes/mentorshipRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const { getMentors } = require('../controllers/menteeController');
-const { getAllConnections } = require('../controllers/mentorshipController');
+const { getAllConnections, requestConnection, updateRequestStatus } = require('../controllers/mentorshipController');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 
 router.get('/mentee/home', isAuthenticated, getMentors);
 router.get('/connections', isAuthenticated, getAllConnections);
+router.post('/request', isAuthenticated, requestConnection);
+router.patch('/request/update', isAuthenticated, updateRequestStatus);
 
 module.exports = router;
