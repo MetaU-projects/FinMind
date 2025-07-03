8ecac8aa11c30f66b3c4fed44c96fecf16c6b729
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-02 23:00:37 -0700

    Created a list component that fetches data from the API and passes each mentor detail as a prop to the card details

---
+++ frontend/src/components/ConnectionsList/ConnectionList.jsx
@@ -0,0 +1,39 @@
+import ConnectionCard from "./ConnectionCard";
+import { getAllConnections } from "../../services/mentorshipService";
+import { useEffect, useState } from "react";
+import { useUser } from "../../contexts/UserContext";
+
+export default function ConnectionList() {
+    const [connections, setConnections] = useState([]);
+    const { user } = useUser();
+
+    useEffect(() => {
+        const fetchConnections = async () => {
+            const results = await getAllConnections(user.role);
+            setConnections(results);
+        }
+        fetchConnections();
+    }, [])
+
+
+    return (
+        <div className="list-container">
+            {user.role === "MENTEE" ? (
+                (connections.map(connection => (
+                    <ConnectionCard
+                        key={connection.mentor.id}
+                        person={connection.mentor}
+                    />
+                )))
+            ) : (
+                (connections.map(connection => (
+                    <ConnectionCard
+                        key={connection.mentee.id}
+                        person={connection.mentee}
+                    />
+                )))
+            )
+            }
+        </div>
+    )
+}
