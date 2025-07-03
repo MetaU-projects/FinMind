d9bbd8ba8644bdd039a2a8d55a0a8187dd824990
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-03 14:22:24 -0700

    Moved the state of the API fetch to a parent component

--- frontend/src/components/ConnectionsList/ConnectionList.jsx
+++ frontend/src/components/ConnectionsList/ConnectionList.jsx
@@ -1,25 +1,12 @@
 import ConnectionCard from "./ConnectionCard";
-import { getAllConnections } from "../../services/mentorshipService";
-import { useEffect, useState } from "react";
-import { useUser } from "../../contexts/UserContext";
 import "./ConnectionList.css"
 
-export default function ConnectionList() {
-    const [connections, setConnections] = useState([]);
-    const { user } = useUser();
-
-    useEffect(() => {
-        const fetchConnections = async () => {
-            const results = await getAllConnections(user.role);
-            setConnections(results);
-        }
-        fetchConnections();
-    }, [])
+export default function ConnectionList({ connections, role }) {
 
 
     return (
         <div className="list-container">
-            {user.role === "MENTEE" ? (
+            {role === "MENTEE" ? (
                 (connections.map(connection => (
                     <ConnectionCard
                         key={connection.mentor.id}
--- frontend/src/pages/Connections/Connections.jsx
+++ frontend/src/pages/Connections/Connections.jsx
@@ -1,15 +1,29 @@
+import { getAllConnections } from "../../services/mentorshipService";
+import { useEffect, useState } from "react";
+import { useUser } from "../../contexts/UserContext";
 import ConnectionList from "../../components/ConnectionsList/ConnectionList";
 import Header from "../../components/Header/Header";
 import "./Connections.css"
 
 export default function Connections() {
+    const [connections, setConnections] = useState([]);
+    const { user } = useUser();
+
+    useEffect(() => {
+        const fetchConnections = async () => {
+            const results = await getAllConnections(user.role);
+            setConnections(results);
+        }
+        fetchConnections();
+    }, [])
+
     return (
         <div className="page-wrapper">
             <Header />
-            <h2 className="connections-count">20 Connections</h2>
+            <h2 className="connections-count">{connections.length} Connections</h2>
             <div className="connections-content">
                 <div className="connections-left">
-                    <ConnectionList />
+                    <ConnectionList connections={connections} role={user.role} />
                 </div>
             </div>
         </div>
