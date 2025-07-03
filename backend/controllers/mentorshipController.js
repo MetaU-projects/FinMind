3ce85c40f025cba4af2fe57a262cbce80225bb7c
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-03 14:01:19 -0700

    Converted if statement to switch statement

--- backend/controllers/mentorshipController.js
+++ backend/controllers/mentorshipController.js
@@ -5,20 +5,23 @@ const getAllConnections = async (req, res) => {
     const userId = req.session.userId;
     const { role } = req.query;
     try {
-        if (role === "MENTOR") {
-            const connections = await prisma.mentorship.findMany({
+        let connections;
+        switch ( role ) {
+        case "MENTOR":
+            connections = await prisma.mentorship.findMany({
                 where: { mentorId: userId },
                 include: { mentee: true }
             })
             return res.status(201).json(connections);
-        }
-        if (role === "MENTEE") {
-            const connections = await prisma.mentorship.findMany({
+        case "MENTEE":
+            connections = await prisma.mentorship.findMany({
                 where: { menteeId: userId },
                 include: { mentor: true }
             })
             return res.status(201).json(connections);
-        }
+        default:
+            res.status(404).json({ error: "Role is invalid!" });
+    }
     } catch (err) {
         console.error(err);
         res.status(404).json({ error: "No active connections" });
@@ -40,44 +43,6 @@ const createMentorship = async (req, res) => {
 
         if (!requestStatus) {
             return res.status(404).json({ error: "Request connection first" });
-        }
-        const connected = await prisma.mentorship.create({ data: { menteeId, mentorId } });
-        res.status(201).json(connected);
-    } catch (err) {
-        console.error(err);
-        res.status(404).json({ error: "Failure to connect!" });
-    }
-}
-
-const updateMentorship = async (req, res) => {
-    const { connectionId, status, endedAt } = req.body;
-    try {
-        const updateConnection = await prisma.request.update({
-            where: { id: connectionId },
-            data: { status, endedAt }
-        });
-        res.status(201).json(updateConnection);
-    } catch (err) {
-        console.error(err);
-        res.status(404).json({ error: "Error updating connection!" });
-    }
-}
-
-const createMentorship = async (req, res) => {
-    const { menteeId, mentorId } = req.body;
-    try {
-        const requestStatus = await prisma.request.findUnique({
-            where: {
-                menteeId_mentorId_status: {
-                    menteeId,
-                    mentorId,
-                    status: "ACCEPTED"
-                }
-            }
-        });
-
-        if (!requestStatus) {
-            return res.status(404).json({ error: "Connection declined" });
         }
         const connected = await prisma.mentorship.create({ data: { menteeId, mentorId } });
         res.status(201).json(connected);
