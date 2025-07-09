ba861dbb10bfec4bcf330650b7ef2f7f0f8111f6
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-09 14:55:20 -0700

    Added custom tooltip for each mentor

--- frontend/src/components/MentorsList/MentorCard.jsx
+++ frontend/src/components/MentorsList/MentorCard.jsx
@@ -6,19 +6,25 @@ export default function MentorCard({ mentor, onConnect }) {
         <div className='card'>
             <div className='card-left'>
                 <div className="card-icon">
-                    <RxAvatar className="avatar" />
-                    <h4>{mentor.classification}</h4>
+                    <div className="tooltip">
+                        <RxAvatar className="avatar" />
+                        <span className="tooltiptext"><strong>Available:</strong> {mentor.availability}</span>
+                    </div>
+
                 </div>
                 <div className="mentor-detail">
                     <div className="top">
-                        <h3>{mentor.name}</h3>
+                        <div className="name-class">
+                            <h3>{mentor.name}</h3>
+                            <p>{mentor.classification}</p>
+                        </div>
                         <div className="skill-tags">
                             {mentor.description.split(', ').map(skill => (
                                 <span key={skill} className="skill-tag">{skill}</span>
                             ))}
                         </div>
                     </div>
-                    <p>{mentor.bio}</p>
+                    <p className="mentor-bio">{mentor.bio}</p>
                 </div>
             </div>
 
