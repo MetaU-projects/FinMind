e03e76b7b4f73055e9246fd7a8317b5d191d68c7
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-17 10:08:31 -0700

    Removed request when hte declined button is pressed

--- frontend/src/components/MenteeRequestsList/RequestList.jsx
+++ frontend/src/components/MenteeRequestsList/RequestList.jsx
@@ -4,6 +4,7 @@ import { useUser } from "../../contexts/UserContext";
 import { getMenteeRequests, requestResponse } from "../../services/mentorService";
 import { createMentorship } from "../../services/mentorshipService";
 import { requestStatus } from "../../utils/status";
+import { removeRequest } from "../../services/menteeService";
 
 export default function RequestList() {
     const [requests, setRequests] = useState([]);
@@ -20,8 +21,12 @@ export default function RequestList() {
     const handleResponse = async (requestId, menteeId, status) => {
         const respondedAt = new Date();
         await requestResponse(requestId, status, respondedAt);
-        if (status === requestStatus.ACCEPTED) {
-            await createMentorship(menteeId, user.id);
+        switch(status) {
+            case requestStatus.ACCEPTED:
+                await createMentorship(menteeId, user.id);
+                break;
+            case requestStatus.DECLINED:
+                await removeRequest(requestId);
         }
         setRequests(requests.filter(request => request.mentee.id !== menteeId));
     }
