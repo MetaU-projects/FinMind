fc92f5c7c03956c045b22161965162e8b1a4e0d4
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-17 23:53:43 -0700

    Added placeholder layout for scheduling and meetings tab

---
+++ frontend/src/components/ConnectionsComp/Meetings.jsx
@@ -0,0 +1,11 @@
+import "./ConnectionsComp.css"
+
+export default function Meetings() {
+    return (
+        <div className="wrapper">
+            <div className="upcoming">Meeting things</div>
+            <div className="history">Meeting things</div>
+            <div className="meeting-stats">Meeting things</div>
+        </div>
+    )
+}
