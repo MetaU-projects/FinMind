61a9aea8a6999748bdb6bbc5ad4e1343aeb06b75
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-17 23:48:42 -0700

    Created components to display three categories of tasks and it's cards

---
+++ frontend/src/components/ConnectionsComp/TaskComps/TaskCard.jsx
@@ -0,0 +1,15 @@
+import "./TaskComps.css";
+
+export default function TaskCard(){
+    return (
+        <div className="task-container">
+            <h4 className="task-title">Presentation slides</h4>
+            <p className="task-description">Create slides for next week's stakeholder meeting</p>
+
+            <div className="task-status">
+                <span className="status">in-progess</span>
+                <span className="status">completed</span>
+            </div>
+        </div>
+    )
+}
