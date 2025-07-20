7746b3e86c06ec954230ebec777324c2517bf2ea
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-19 21:53:10 -0700

    Created a constant config file that stores constant for the whole database

---
+++ backend/config/constants.js
@@ -0,0 +1,11 @@
+const DAYS_IN_WEEK = 7;
+const ONE_HOUR_SECONDS = 3600;
+const MS_PER_SECOND = 1000;
+const TOP_NUMBER = 5;
+
+module.exports = {
+    DAYS_IN_WEEK,
+    ONE_HOUR_SECONDS,
+    MS_PER_SECOND,
+    TOP_NUMBER 
+}
