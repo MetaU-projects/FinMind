335511c9f5505df99dfbc317593bdad1719d212f
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-19 21:59:29 -0700

    Replaced unrelated change in PR

--- frontend/src/components/MentorsList/MentorCard.jsx
+++ frontend/src/components/MentorsList/MentorCard.jsx
@@ -1,34 +1,32 @@
+import { FaUserPlus } from "react-icons/fa";
 import { RxAvatar } from "react-icons/rx";
 import './MentorCard.css';
 
 export default function MentorCard({ mentor, onConnect }) {
     return (
         <div className='card'>
-            <div className='card-left'>
-                <div className="card-icon">
-                    <div className="tooltip">
-                        <RxAvatar className="avatar" />
-                        <span className="tooltiptext"><strong>Available:</strong> {mentor.availability}</span>
-                    </div>
-
+            <div className="card-content">
+                <div className="tooltip">
+                    <img
+                        src={`https://randomuser.me/api/portraits/women/${mentor.id + 20}.jpg`}
+                        alt={mentor.name + "profile"}
+                        className="profile-img"
+                    />
+                    <span className="tooltiptext"><strong>Available:</strong> {mentor.availability}</span>
                 </div>
-                <div className="mentor-detail">
-                    <div className="top">
-                        <div className="name-class">
-                            <h3>{mentor.name}</h3>
-                            <p>{mentor.classification}</p>
-                        </div>
-                        <div className="skill-tags">
-                        </div>
-                    </div>
-                    <p className="mentor-bio">{mentor.bio}</p>
+
+                <div className="card-name">
+                    <h3>{mentor.name}</h3>
+                    <p>{mentor.classification} {mentor.major}</p>
                 </div>
             </div>
-
-            <div className="card-right">
-                <p>{mentor.major}</p>
-                <button className="connect-btn" onClick={onConnect}>Connect</button>
+            <p className="card-bio">{mentor.bio}</p>
+            <div className="card-skills">
+                {mentor.description.split(', ').map(skill => (
+                    <span key={skill} className="card-skill-tag">{skill}</span>
+                ))}
             </div>
+            <button className="btn" onClick={onConnect}><FaUserPlus className="connect-icon" />Connect</button>
         </div>
     )
 }
