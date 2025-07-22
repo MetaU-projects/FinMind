c6923beefb102db0da7cc2a68cc4b2aef22218fd
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-22 16:02:23 -0700

    Updated layout of each task card to include card actions such as changing the task status

--- frontend/src/components/ConnectionsComp/TaskComps/TaskCard.jsx
+++ frontend/src/components/ConnectionsComp/TaskComps/TaskCard.jsx
@@ -1,14 +1,58 @@
+import { useState } from "react";
 import "./TaskComps.css";
+import { AiOutlineEllipsis } from "react-icons/ai";
+import { taskStatus } from "../../../utils/status";
+import { updateTask } from "../../../services/taskService";
+import ErrorModal from "../../ErrorModal/ErrorModal";
 
-export default function TaskCard(){
+export default function TaskCard({ task }) {
+    const [action, setAction] = useState(false);
+    const [error, setError] = useState("")
+
+    const handleTaskAction = async (value) => {
+        let taskAction;
+        switch(value){
+            case "todo":
+                taskAction = taskStatus.TODO;
+                break;
+            case "inprogress":
+                taskAction = taskStatus.INPROGRESS;
+                break;
+            case "complete":
+                taskAction = taskStatus.COMPLETE;
+                break;
+        }
+        setAction(false);
+        try {
+            const data = await updateTask(task.id, taskAction);
+        } catch(err) {
+            setError(err.message)
+        }
+    }
+
+    
+
     return (
-        <div className="task-container">
-            <h4 className="task-title">Presentation slides</h4>
-            <p className="task-description">Create slides for next week's stakeholder meeting</p>
-
-            <div className="task-status">
-                <span className="status">in-progess</span>
-                <span className="status">completed</span>
+        <div>
+            {error && (<ErrorModal setError={setError} error={error} />)}
+            <div className="task-container relative">
+                <div className="task-top-text">
+                    <h4 className="task-title">{task.title}</h4>
+                    <AiOutlineEllipsis className="task-actions" onClick={() => setAction(true)} />
+                </div>
+                {action &&
+                    <select className="action-dropdown" onChange={(e) => handleTaskAction(e.target.value)}>
+                        <option value="">Mark as</option>
+                        <option value="todo">todo</option>
+                        <option value="inprogress">in progress</option>
+                        <option value="complete">complete</option>
+                    </select>
+                }
+                <p className="task-description">{task.description}</p>
+                <div className="task-status">
+                    <span className="status">{task.status}</span>
+                    <span className="priority">{task.priority}</span>
+                </div>
             </div>
         </div>
     )
