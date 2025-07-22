c85f683d05c00a5c79b500d32df71fc32dd172e3
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-22 15:59:24 -0700

    Updated the Tasks tab to fetch mentorship tasks data from the API, filtered based on status and passed as props to TaskColumn

--- frontend/src/components/ConnectionsComp/Tasks.jsx
+++ frontend/src/components/ConnectionsComp/Tasks.jsx
@@ -1,12 +1,36 @@
 import { AiOutlinePlus } from "react-icons/ai";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NewTaskModal from "./TaskComps/NewTaskModal";
 import TaskColumn from "./TaskComps/TaskColumn";
+import { getTasks } from "../../services/taskService";
+import { taskStatus } from "../../utils/status";
+import ErrorModal from "../ErrorModal/ErrorModal";
 
 export default function Tasks({ connection }) {
     const [addTask, setAddTask] = useState(false);
+    const [todo, setTodo] = useState([]);
+    const [inProgess, setInProgess] = useState([]);
+    const [complete, setComplete] = useState([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getTasks(connection.id);
+                setTodo(data.filter(item => item.status === taskStatus.TODO));
+                setInProgess(data.filter(item => item.status === taskStatus.INPROGRESS));
+                setComplete(data.filter(item => item.status === taskStatus.COMPLETE));
+            } catch (err) {
+                setError(err.message);
+            }
+        }
+        fetchData();
+    });
+
     return (
         <div>
+            {error && <ErrorModal error={error} setError={setError} />}
+
             <div className="task-top">
                 <div className="task-text">
                     <h2>Tasks</h2>
@@ -16,13 +40,13 @@ export default function Tasks({ connection }) {
             </div>
 
             <div className="task-cards">
-                <TaskColumn />
-                <TaskColumn />
-                <TaskColumn />
+                <TaskColumn value="ToDo" taskList={todo} />
+                <TaskColumn value="In Progress" taskList={inProgess} />
+                <TaskColumn value="Completed" taskList={complete} />
             </div>
             {addTask &&
                 <NewTaskModal
-                connection={connection}
+                    connection={connection}
                     setAddTask={setAddTask}
                 />
             }
