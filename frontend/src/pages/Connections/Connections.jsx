84f5f33baa8e5734e5b6ecf384b32518cadd9f62
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-22 01:52:34 -0700

    Removed console log

--- frontend/src/pages/Connections/Connections.jsx
+++ frontend/src/pages/Connections/Connections.jsx
@@ -35,7 +35,6 @@ export default function Connections() {
         setUpcoming(upComingData);
         setMeetingHistory(historyData);
     }
-    console.log(upComing)
 
     return (
         <div className="page">
