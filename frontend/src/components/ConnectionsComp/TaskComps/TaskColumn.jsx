3901191f117e885d4bf9685ee40c61a68b1e1d22
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-22 16:00:56 -0700

    Updated TaskColumn component card task placeholder to results from the DB

--- frontend/src/components/ConnectionsComp/TaskComps/TaskColumn.jsx
+++ frontend/src/components/ConnectionsComp/TaskComps/TaskColumn.jsx
@@ -1,16 +1,16 @@
 import TaskCard from "./TaskCard";
 import "./TaskComps.css";
 
-export default function TaskColumn(){
+export default function TaskColumn({ value, taskList }) {
     return (
         <div className="col-container">
-            <h3 className="col-title"> Todo(s) </h3>
+            <h3 className="col-title"> {value}(s) : {taskList.length} </h3>
             <div className="col-cards">
-                <TaskCard />
-                <TaskCard />
-                <TaskCard />
+                {taskList.map(task => (
+                    <TaskCard task={task} />
+                ))}
             </div>
         </div>
     )
-    
+
 }
