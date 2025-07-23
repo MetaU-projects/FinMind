f25ef0e360150c63aa42373cd981b4d12a37d4d1
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-23 16:45:44 -0700

    Added styling for empty cases

--- frontend/src/components/MenteeHomeComp/Recommendations.jsx
+++ frontend/src/components/MenteeHomeComp/Recommendations.jsx
@@ -11,7 +11,7 @@ export default function Recommended ({ recommend }) {
                         </div>
                     </li>
                 ))) : (
-                    <div>Looking for recommnedations for you</div>
+                    <div className="side-empty">Looking for recommnedations for you</div>
                 )}
             </ul>
         </div>
