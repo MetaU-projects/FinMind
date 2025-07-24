325bdf371f32418442666b5a02ea2bd20b28c5ea
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-23 22:52:08 -0700

    Created function for fuzzy search and controller logic to return search results

---
+++ backend/utils/fuzzySearch.js
@@ -0,0 +1,21 @@
+const Fuse = require('fuse.js');
+
+/**
+ * 
+ * @param {Array} users - Array of user objects from the DB
+ * @param {*} query - The search term inputted by the user.
+ * @returns 
+ */
+
+const fuzzySearch = (users, query) => {
+    const fuse = new Fuse(users, {
+        keys: ['name', 'school', 'major',  'interests'],
+        threshold: 0.3,
+        includeScore: true
+    });
+
+    const results = fuse.search(query);
+    return results.map(res => res.item);
+}
+
+module.exports = { fuzzySearch }
