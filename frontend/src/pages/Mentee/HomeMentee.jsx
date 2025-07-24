a07f3706c471c5765dd78e7a224e3c89fc56f2fe
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-23 22:56:50 -0700

    Updated the banner to have search and added functionality to search for mentors based on criteria

--- frontend/src/pages/Mentee/HomeMentee.jsx
+++ frontend/src/pages/Mentee/HomeMentee.jsx
@@ -1,7 +1,7 @@
 import Header from "../../components/Header/Header";
 import PendingRequests from "../../components/MenteeHomeComp/PendingRequests";
 import MentorList from "../../components/MentorsList/MentorsList";
-import { getAllPendingRequests, getAvailableMentors, getRecommendedMentors } from "../../services/menteeService";
+import { getAllPendingRequests, getAvailableMentors, getRecommendedMentors, searchMentors } from "../../services/menteeService";
 import { useState, useEffect } from "react";
 import "./HomeMentee.css";
 import ToolBar from "../../components/MenteeHomeComp/ToolBar";
@@ -10,10 +10,10 @@ import Recommended from "../../components/MenteeHomeComp/Recommendations";
 export default function HomeMentee() {
     const [pendingRequests, setPendingRequests] = useState([]);
     const [mentors, setMentors] = useState([]);
-    const [recommend, setRecommend] = useState([])
+    const [recommend, setRecommend] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
-    useEffect(() => {
-        const fetchData = async () => {
+    const fetchData = async () => {
             const results = await getAvailableMentors();
             const pendingResults = await getAllPendingRequests();
             const recomData = await getRecommendedMentors();
@@ -21,28 +21,38 @@ export default function HomeMentee() {
             setMentors(results);
             setRecommend(recomData);
         }
+ 
+
+    const handleMentorSearch = async(query) => {
+        const data = await searchMentors(query);
+        setMentors(data);
+    }
+
+    useEffect(() => {
         fetchData();
     }, []);
 
     return (
-        <div>
-            <Header />
+        <div className="home-page">
+            <div className="home-header">
+                <Header />
+                <ToolBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} onSearch={handleMentorSearch} setMentors={setMentors} getData={fetchData} />
+            </div>
             <div className="home-content">
                 <div className="home-left">
-                    <ToolBar />
-                    <MentorList
-                        mentors={mentors}
-                        setMentors={setMentors}
-                        setPendingRequests={setPendingRequests}
-                    />
-                </div>
-                <div className="home-right">
                     <PendingRequests
                         pendingRequests={pendingRequests}
                         setMentors={setMentors}
                         setPendingRequests={setPendingRequests}
                     />
                     <Recommended recommend={recommend} />
+                </div>
+                <div className="home-right">
+                    <MentorList
+                        mentors={mentors}
+                        setMentors={setMentors}
+                        setPendingRequests={setPendingRequests}
+                    />
                 </div>
             </div>
         </div>
