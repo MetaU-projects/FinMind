85bc4f09c9f43d62eec6f8de71e048aeb75b0186
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-25 09:36:52 -0700

    Updated constant configuration for better weights scores

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -2,14 +2,20 @@ const DAYS_IN_WEEK = 7;
 const ONE_HOUR_SECONDS = 3600;
 const MS_PER_SECOND = 1000;
 const TOP_NUMBER = 5;
-const SCORE_BASE = 1;
-const SCORE_MORE = 2
+
+const WEIGHTS = {
+    bio: 4.0,
+    interest: 3.0,
+    graph: 2.0,
+    major: 1.5,
+    school: 1.0,
+    bonus: 1.5
+}
 
 module.exports = {
     DAYS_IN_WEEK,
     ONE_HOUR_SECONDS,
     MS_PER_SECOND,
     TOP_NUMBER,
-    SCORE_BASE,
-    SCORE_MORE 
+    WEIGHTS 
 }
