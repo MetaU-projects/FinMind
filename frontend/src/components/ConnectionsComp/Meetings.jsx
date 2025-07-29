ca7362652d3d91cec8af89058ce39abb9e9af034
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-29 14:41:04 -0700

    Updated meetings tab styling for mobile responsiveness

--- frontend/src/components/ConnectionsComp/Meetings.jsx
+++ frontend/src/components/ConnectionsComp/Meetings.jsx
@@ -48,7 +48,7 @@ export default function Meetings({ upComing, setUpcoming, meetingHistory, connec
                                         <p>{formatUnixTimes(meeting.startTime, meeting.endTime)}</p>
                                     </div>
                                 </div>
-                                <button className="text-red-500 cursor-pointer" onClick={() => handleSessionCancel(meeting.id)}>Cancel</button>
+                                <button className="cancel-session" onClick={() => handleSessionCancel(meeting.id)}>Cancel</button>
                                 <span className="upcoming-status">
                                     <AiOutlineWechat />
                                     <a href={`mailto:${connection[role].email}`}>Send Email</a>
