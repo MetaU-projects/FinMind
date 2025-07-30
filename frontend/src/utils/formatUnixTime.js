34dfc34d64e91197c6264be9e1459260a52b1449
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-07-30 09:55:53 -0700

    Updated timezone

--- frontend/src/utils/formatUnixTime.js
+++ frontend/src/utils/formatUnixTime.js
@@ -1,8 +1,15 @@
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const userTimeZone = Int1.DateTimeFormat().resolvedOptions().timezone;
 
 export const formatUnixTimes = (startTime, endTime) => {
-    const start = dayjs.unix(startTime);
-    const end = dayjs.unix(endTime);
+    const start = dayjs.unix(startTime).tz(userTimeZone);
+    const end = dayjs.unix(endTime).tz(userTimeZone);
 
     return `${start.format('MM/DD/YYYY h:mm A')} - ${end.format('h:mm A')}`;
 }
