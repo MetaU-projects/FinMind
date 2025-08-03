9e86388f830cce02f8199916d8118904dd8cfe6d
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-08-03 11:29:59 -0700

    Updated mentorship deletion

--- backend/controllers/mentorshipController.js
+++ backend/controllers/mentorshipController.js
@@ -85,13 +85,9 @@ const endMentorship = async (req, res) => {
             await prisma.request.delete({ where: { id: request.id } });
         }
 
-        const tasks = await prisma.task.findFirst({
+        const tasks = await prisma.task.delete({
             where: { mentorshipId: connectionId }
         });
-
-        if(tasks) {
-            await prisma.task.delete({ where: {id: tasks.id} })
-        }
 
         await prisma.mentorship.delete({ where: { id: connectionId } });
         
