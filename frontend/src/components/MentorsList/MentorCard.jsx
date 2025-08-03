05c5d0c36e3a90a4ec4d71501f4f715e001e15a6
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2025-08-03 11:49:17 -0700

    Merging changes

--- frontend/src/components/MentorsList/MentorCard.jsx
+++ frontend/src/components/MentorsList/MentorCard.jsx
@@ -24,14 +24,14 @@ export default function MentorCard({ mentor, onConnect, onSelect }) {
                         <p>{mentor.classification} {mentor.major}</p>
                     </div>
                 </div>
-                <p className="card-bio" onClick={() => onSelect(mentor)}>{mentor.bio}</p>
-                <div className="card-skills">
-                    {Array.isArray(mentor.interest) && mentor.interest.map(skill => (
-                        <span key={mentor.interest.id}>{skill.interest.name}</span>
-                    ))}
-                </div>
-                <button className="btn" onClick={onConnect}><FaUserPlus className="connect-icon" /> Connect</button>
             </div>
+            <p className="card-bio">{mentor.bio}</p>
+            <div className="card-skills">
+                {mentor.interest.map(skill => (
+                    <span key={skill.id}>{skill.interest.name}</span>
+                ))}
+            </div>
+            <button className="btn" onClick={onConnect}><FaUserPlus className="connect-icon" /> Connect</button>
         </div>
     )
 }
